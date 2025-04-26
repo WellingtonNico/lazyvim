b361f839a6169c5e468b332ecda87631cf70aac4
WellingtonNico <wellingtonicoletti@gmail.com>
2025-04-26 20:33:23 -0300

    add: enable spell checking and set languages in init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.spell = true -- Enable spell checking
+vim.o.spelllang = 'en,pt' -- Set spell languages to English and Portuguese
