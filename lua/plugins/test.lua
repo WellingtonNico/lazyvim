98c167b21439856c3c821f364f2f41f87426ced8
WellingtonNico <wellingtonicoletti@gmail.com>
2025-04-26 18:33:09 -0300

    add configuração tests vitest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,34 +1,34 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/neotest-python",
-    -- "Issafalcon/neotest-dotnet",
-  },
-  opts = function(_, opts)
-    opts.adapters = opts.adapters or {}
+    "nvim-neotest/neotest",
+    dependencies = {"nvim-neotest/neotest-python", "marilari88/neotest-vitest" -- "Issafalcon/neotest-dotnet",
+    },
+    opts = function(_, opts)
+        opts.adapters = opts.adapters or {}
 
-    -- Add neotest-python
-    table.insert(
-      opts.adapters,
-      require("neotest-python")({
-        dap = { justMyCode = false },
-        args = { "--log-level", "DEBUG" },
-        runner = "pytest",
-        python = ".venv/bin/python", -- Adjust if needed
-        pytest_discover_instances = true,
-      })
-    )
+        -- Add neotest-python
+        table.insert(opts.adapters, require("neotest-python")({
+            dap = {
+                justMyCode = false
+            },
+            args = {"--log-level", "DEBUG"},
+            runner = "pytest",
+            python = ".venv/bin/python", -- Adjust if needed
+            pytest_discover_instances = true
+        }))
 
-    -- Add neotest-dotnet
-    -- table.insert(
-    --   opts.adapters,
-    --   require("neotest-dotnet")({
-    --     strategy = "integrated",
-    --     dap = {
-    --       -- Optional settings for DAP
-    --       justMyCode = false,
-    --     },
-    --   })
-    -- )
-  end,
+        -- Add neotest-vitest
+        table.insert(opts.adapters, require("neotest-vitest"))
+
+        -- Add neotest-dotnet
+        -- table.insert(
+        --   opts.adapters,
+        --   require("neotest-dotnet")({
+        --     strategy = "integrated",
+        --     dap = {
+        --       -- Optional settings for DAP
+        --       justMyCode = false,
+        --     },
+        --   })
+        -- )
+    end
 }
