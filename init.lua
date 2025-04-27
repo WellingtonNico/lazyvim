393f2ee56f452745e3c39aa3543c10f29fcb820a
WellingtonNico <wellingtonicoletti@gmail.com>
2025-04-26 21:09:19 -0300

    remove: disable spell checking and language settings in init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.o.spell = true -- Enable spell checking
-vim.o.spelllang = 'en,pt' -- Set spell languages to English and Portuguese
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.spell = true -- Enable spell checking
+vim.o.spelllang = 'en,pt' -- Set spell languages to English and Portuguese
